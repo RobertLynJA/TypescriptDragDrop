b928d72a08a51dfb461493c68b816e64249074f0
Robert Lyn <lenny4422@gmail.com>
2022-05-27 16:38:46 -0400

    Get Input elements and setup submit handler.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement:  HTMLElement;
     element: HTMLFormElement;
+    titleInput: HTMLInputElement;
+    descriptionInput: HTMLInputElement;
+    peopleInput: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -10,7 +13,21 @@ class ProjectInput {
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
         this.element.id = 'user-input';
+
+        this.titleInput = this.element.querySelector('#title')! as HTMLInputElement;
+        this.descriptionInput = this.element.querySelector('#description')! as HTMLInputElement;
+        this.peopleInput = this.element.querySelector('#people')! as HTMLInputElement;
+
+        this.configure();
         this.attach();
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler)
     }
 
     private attach() {
