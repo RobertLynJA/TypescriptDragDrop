a7eac38db48d0c63ee7e12c3388f12818a7c6b15
Robert Lyn <lenny4422@gmail.com>
2022-05-30 20:07:29 -0400

    Added project state

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,31 @@
+class ProjectState {
+    private projects: any[] = [];
+    private static instance: ProjectState;
+
+    private constructor() { }
+
+    static getInstance() {
+        if (!this.instance) {
+            this.instance = new ProjectState();
+        }
+
+        return this.instance;
+    }
+
+    addProject(title: string, description: string, numOfPeople: number) {
+        const newProject = {
+            id: Math.random().toString(),
+            title: title, 
+            description: description,
+            people: numOfPeople
+        };
+
+        this.projects.push(newProject);
+    }
+}
+
+const projectState = ProjectState.getInstance();
+
 function autobind(_target: any, _methodName: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
     const adjDescriptor: PropertyDescriptor = {
