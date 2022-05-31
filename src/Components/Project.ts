a324f82286f183c7ce25750b72ac345c97ee3a60
Robert Lyn <lenny4422@gmail.com>
2022-05-31 14:20:41 -0400

    Setup ES6 components #2

---
+++ src/Components/Project.ts
@@ -0,0 +1,7 @@
+import { ProjectStatus } from '../enums/ProjectStatus.js';
+
+export class Project {
+    constructor(public id: string, public title: string,
+         public description: string, public people: number,
+         public status: ProjectStatus) {}
+}
--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,7 @@
-interface Draggable {
-    dragStartHandler(event: DragEvent): void;
-    dragEndHandler(event: DragEvent): void;
-}
-
-interface DragTarget {
-    dragOverHandler(event: DragEvent): void;
-    dropHandler(event: DragEvent): void;
-    dragLeaveHandler(event: DragEvent): void;
-}
-
-enum ProjectStatus {
-    Active,
-    Finished
-}
-
-class Project {
-    constructor(public id: string, public title: string,
-         public description: string, public people: number,
-         public status: ProjectStatus) {}
-}
+import { Draggable } from './interfaces/Draggable.js';
+import { DragTarget } from './interfaces/DragTarget.js';
+import { ProjectStatus } from './enums/ProjectStatus.js';
+import { Project } from './Components/Project.js';
 
 type Listener<T> = (items: T[]) => void;
 
@@ -340,6 +323,6 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     }
 }
 
-const prjInput = new ProjectInput();
-const activePrjList = new ProjectList('active');
-const finishedPrjList = new ProjectList('finished');
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
