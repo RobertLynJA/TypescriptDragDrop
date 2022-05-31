68b9693f9401e27b86d32715ebd8eed5cdf6319b
Robert Lyn <lenny4422@gmail.com>
2022-05-30 20:54:46 -0400

    Filter lists properly. Prevent duplicates on add.

--- src/app.ts
+++ src/app.ts
@@ -107,7 +107,14 @@ class ProjectList {
         this.element.id = `${this.type}-projects`;
 
         projectState.addListener((projects: Project[]) => {
-            this.assignedProjects = projects;
+            const relevantProjects = projects.filter(prj => {
+                if (this.type === 'active') {
+                    return prj.status === ProjectStatus.Active;
+                } 
+
+                return prj.status === ProjectStatus.Finished;
+            })
+            this.assignedProjects = relevantProjects;
             this.renderProjects();
         });
 
@@ -117,6 +124,8 @@ class ProjectList {
 
     private renderProjects() {
         const listEl = document.getElementById(`${this.type}-projects-list`)!;
+
+        listEl.innerHTML = '';
 
         for (const projItem of this.assignedProjects) {
             const listItem = document.createElement('li');
